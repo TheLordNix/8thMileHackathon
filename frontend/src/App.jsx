095e3400b6e9476bef6c80d7fae52791baf4b061
aYush cHanduka <76642200+AyushChanduka9@users.noreply.github.com>
2025-12-05 00:13:52 +0530

    added firebase

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,10 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import { getDatabase } from "firebase/database"
+import { app } from "firebase"
+
+const db = getDatabase(app);
 
 function App() {
   const [count, setCount] = useState(0)
