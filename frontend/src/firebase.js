fdad53badaeaf91ba0e85d4dd8c10b01a167acea
aYush cHanduka <76642200+AyushChanduka9@users.noreply.github.com>
2025-12-07 23:16:08 +0530

    Remove Firebase configuration from firebase.js
    Removed Firebase configuration details from firebase.js.

--- frontend/src/firebase.js
+++ frontend/src/firebase.js
@@ -4,16 +4,6 @@ import { getDatabase } from "firebase/database";
 import { getAuth } from "firebase/auth";
 
 // Your Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyDzyiqB9HMNE-nwzqRD9jbfiVc4zFZQL34",
-  authDomain: "thmile-3d308.firebaseapp.com",
-  projectId: "thmile-3d308",
-  storageBucket: "thmile-3d308.firebasestorage.app",
-  messagingSenderId: "914372109190",
-  appId: "1:914372109190:web:767439b980b5485abc8207",
-  measurementId: "G-F0KXZFCTQ7",
-  databaseURL: "https://thmile-3d308-default-rtdb.firebaseio.com/",
-};
 
 // Initialize Firebase App
 export const app = initializeApp(firebaseConfig);
